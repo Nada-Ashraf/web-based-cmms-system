2524f9d9f3b5aa4042f551d9d50e445decbbcef5
nada-ashraf <nadaashraf11@icloud.com>
2020-04-27 22:15:50 +0200

    Added get asset routes

--- routes/asset.js
+++ routes/asset.js
@@ -3,28 +3,45 @@ const router = express.Router();
 const { Asset } = require("../models/asset");
 const _ = require("lodash");
 
-// routes
-
-// Add Equipment
+// Add Asset
 router.post("/add_asset", async (req, res) => {
-  equipment = new Asset(
-    _.pick(req.body, [
-      "name",
-      "number",
-      "serial_number",
-      "model",
-      "department",
-      "price",
-      "location",
-      "supplier",
-      "condition",
-      "last_pm_date",
-      "last_failure_date",
-      "last_fix_date",
-    ])
-  );
-  await equipment.save();
-  res.send(equipment);
+  let asset = await Asset.findOne({
+    serial_number: req.body.serial_number,
+  });
+  if (asset) {
+    return res.status(400).send("This asset already exisits!");
+  } else {
+    asset = new Asset(
+      _.pick(req.body, [
+        "name",
+        "serial_number",
+        "model",
+        "department",
+        "price",
+        "location",
+        "supplier",
+        "condition",
+        "last_pm_date",
+        "last_failure_date",
+        "last_fix_date",
+      ])
+    );
+    await asset.save();
+    res.send(asset);
+  }
+});
+
+// View All assets
+router.get("/view_assets", async (req, res) => {
+  const assets = await Asset.find();
+  res.send(assets);
+});
+
+// View details of one asset
+router.get("/view_assets/:id", async (req, res) => {
+  const asset = await Asset.find({ serial_number: req.params.id });
+  res.send(asset);
+  console.log(asset);
 });
 
 module.exports = router;
