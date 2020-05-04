4a3c91236a1ddbd470aa7e1a19cb3ca93ee7d8bc
nada-ashraf <nadaashraf11@icloud.com>
2020-05-04 22:24:01 +0200

    Fetched assets from api

--- frontend/src/views/Assets/Assets.js
+++ frontend/src/views/Assets/Assets.js
@@ -1,101 +1,51 @@
 import React, { Component } from "react";
-import {
-  Badge,
-  Card,
-  CardBody,
-  CardHeader,
-  Pagination,
-  PaginationItem,
-  PaginationLink,
-  Table,
-} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Card, CardBody, CardHeader, Table } from "reactstrap";
 
 class Assets extends Component {
+  state = { assets: [] };
+
+  componentDidMount() {
+    fetch("/api/assets")
+      .then((res) => res.json())
+      .then((assets) => this.setState({ assets }));
+  }
+
   render() {
+    // const userList = usersData.filter((user) => user.id < 10);
     return (
-      <Card>
-        <CardHeader>
-          <i className="fa fa-align-justify"></i> Assets Table
-        </CardHeader>
-        <CardBody>
-          <Table responsive striped>
-            <thead>
-              <tr>
-                <th>Username</th>
-                <th>Date registered</th>
-                <th>Role</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Yiorgos Avraamu</td>
-                <td>2012/01/01</td>
-                <td>Member</td>
-                <td>
-                  <Badge color="success">Active</Badge>
-                </td>
-              </tr>
-              <tr>
-                <td>Avram Tarasios</td>
-                <td>2012/02/01</td>
-                <td>Staff</td>
-                <td>
-                  <Badge color="danger">Banned</Badge>
-                </td>
-              </tr>
-              <tr>
-                <td>Quintin Ed</td>
-                <td>2012/02/01</td>
-                <td>Admin</td>
-                <td>
-                  <Badge color="secondary">Inactive</Badge>
-                </td>
-              </tr>
-              <tr>
-                <td>Enéas Kwadwo</td>
-                <td>2012/03/01</td>
-                <td>Member</td>
-                <td>
-                  <Badge color="warning">Pending</Badge>
-                </td>
-              </tr>
-              <tr>
-                <td>Agapetus Tadeáš</td>
-                <td>2012/01/21</td>
-                <td>Staff</td>
-                <td>
-                  <Badge color="success">Active</Badge>
-                </td>
-              </tr>
-            </tbody>
-          </Table>
-          <Pagination>
-            <PaginationItem disabled>
-              <PaginationLink previous tag="button">
-                Prev
-              </PaginationLink>
-            </PaginationItem>
-            <PaginationItem active>
-              <PaginationLink tag="button">1</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink tag="button">2</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink tag="button">3</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink tag="button">4</PaginationLink>
-            </PaginationItem>
-            <PaginationItem>
-              <PaginationLink next tag="button">
-                Next
-              </PaginationLink>
-            </PaginationItem>
-          </Pagination>
-        </CardBody>
-      </Card>
+      <div className="animated fadeIn">
+        <Card>
+          <CardHeader>
+            <i className="fa fa-align-justify"></i> Assets{" "}
+            <small className="text-muted">list</small>
+          </CardHeader>
+          <CardBody>
+            <Table responsive hover>
+              <thead>
+                <tr>
+                  <th scope="col">id</th>
+                  <th scope="col">name</th>
+                  <th scope="col">registered</th>
+                  <th scope="col">role</th>
+                  <th scope="col">status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.assets.map((asset) => (
+                  <tr key={asset.serial_number}>
+                    <td>{asset.name}</td>
+                    <td>{asset.serial_number}</td>
+                    <td>{asset.price}</td>
+                    <td>@mdo</td>
+                    <td>@mdo</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </CardBody>
+        </Card>
+      </div>
     );
   }
 }
