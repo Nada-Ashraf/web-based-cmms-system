a9fc5b62d98789f18aa5cdea8633774e8d684ad1
nada-ashraf <nadaashraf11@icloud.com>
2020-05-07 03:07:46 +0200

    Added details tabs

--- frontend/src/views/Assets/Asset.js
+++ frontend/src/views/Assets/Asset.js
@@ -1,9 +1,90 @@
 import React, { Component } from "react";
-import { Card, CardBody, CardHeader, Col, Row, Table } from "reactstrap";
-
+import {
+  Badge,
+  Col,
+  Nav,
+  NavItem,
+  NavLink,
+  Row,
+  TabContent,
+  TabPane,
+  Card,
+  CardBody,
+  CardHeader,
+} from "reactstrap";
+import classnames from "classnames";
+// this.props.match.params.id
 class Asset extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      activeTab: new Array(4).fill("1"),
+    };
+  }
+
+  lorem() {
+    return "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit. ";
+  }
+
+  toggle(tabPane, tab) {
+    const newArray = this.state.activeTab.slice();
+    newArray[tabPane] = tab;
+    this.setState({
+      activeTab: newArray,
+    });
+  }
+
+  tabPane() {
+    return (
+      <>
+        <TabPane tabId="1">{`1. ${this.lorem()}`}</TabPane>
+        <TabPane tabId="2">{`2. ${this.lorem()}`}</TabPane>
+        <TabPane tabId="3">{`3. ${this.lorem()}`}</TabPane>
+      </>
+    );
+  }
   render() {
-    return <h1>{this.props.match.params.id}</h1>;
+    return (
+      <div>
+        <Nav tabs>
+          <NavItem>
+            <NavLink
+              active={this.state.activeTab[0] === "1"}
+              onClick={() => {
+                this.toggle(0, "1");
+              }}
+            >
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              active={this.state.activeTab[0] === "2"}
+              onClick={() => {
+                this.toggle(0, "2");
+              }}
+            >
+              Profile
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              active={this.state.activeTab[0] === "3"}
+              onClick={() => {
+                this.toggle(0, "3");
+              }}
+            >
+              Messages
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab[0]}>
+          {this.tabPane()}
+        </TabContent>
+      </div>
+    );
   }
 }
 
