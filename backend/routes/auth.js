557936e1b15af30c8b7ea7a38035e6dce112b946
nada-ashraf <nadaashraf11@icloud.com>
2020-05-10 01:06:08 +0200

    created load user action

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -105,14 +105,14 @@ router.post("/login", async (req, res) => {
  * @access  Private
  */
 
-// router.get("/user", auth, async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user.id).select("-password");
-//     if (!user) throw Error("User Does not exist");
-//     res.json(user);
-//   } catch (e) {
-//     res.status(400).json({ msg: e.message });
-//   }
-// });
+router.get("/user", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) throw Error("User Does not exist");
+    res.json(user);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
+});
 
 module.exports = router;
